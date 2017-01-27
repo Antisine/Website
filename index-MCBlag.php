7821c0b5411e19714af8d2648485e38f1ffaa27d
Joshua Merrill <jsmerrill2004@gmail.com>
2017-01-27 07:28:44 -0800

    Change formatting of MC-Blag

---
+++ index-MCBlag.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Minecraft | Nebula Development | Blog</title>
+  </head>
+  <body>
+      <p>There's nothing here yet!</p>
+  </body>
+</html>
--- index-MCBlog.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>Minecraft | Nebula Development | Blog</title>
-    </head>
-    <body>There's nothing here yet!</body>
-</html>
