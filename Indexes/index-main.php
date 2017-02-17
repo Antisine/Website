c74e19c9136431237f7abf0057657b5c1d6b1fd3
Joshua Merrill <jsmerrill2004@gmail.com>
2017-02-16 16:46:00 -0800

    Added the rest of the indexes from the sites
    Added the index from minecraft.greyball.tk,
    minecraft.greyball.tk/basicSurvival, and deltafour.greyball.tk.

---
+++ Indexes/index-main.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang='en-us'>
+    <head>
+        <meta charset='utf-8'/>
+        <link rel="icon" href="https://cdn.rawgit.com/GreyballDevelopment/Website/master/Greyball%201568x1568.ico" type="image/x-icon">
+        <link rel="stylesheet" href="https://cdn.rawgit.com/GreyballDevelopment/Website/master/main.css" type='text/css'>
+        <title>Welcome - Greyball</title>
+        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <script>
+          (adsbygoogle = window.adsbygoogle || []).push({
+            google_ad_client: "ca-pub-2683826858268042",
+            enable_page_level_ads: true
+          });
+        </script>
+    </head>
+    <body>
+        <p>Welcome to Greyball! You can find code for some of our games on <a href='https://github.com/GreyballDevelopment'>GitHub</a>.</p>
+        <footer>
+            <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+            <!-- Responsive ad 1 -->
+            <ins class="adsbygoogle"
+                 style="display:block"
+                 data-ad-client="ca-pub-2683826858268042"
+                 data-ad-slot="8540436210"
+                 data-ad-format="auto"></ins>
+            <script>
+                (adsbygoogle = window.adsbygoogle || []).push({});
+            </script>
+        </footer>
+    </body>
+</html>
