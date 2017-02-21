2e18864286bb7265bd766658d269ba9c94ddaee3
Joshua Merrill <jsmerrill2004@gmail.com>
2017-02-20 21:15:41 -0800

    Fix some requirements
    Add all the meta tags
    Change where the icon points to

--- Indexes/index-MCBlag.php
+++ Indexes/index-MCBlag.php
@@ -2,6 +2,12 @@
 <html>
   <head>
     <meta charset="utf-8">
+    <link rel="icon" href="https://cdn.rawgit.com/GreyballDevelopment/Website/master/Icons/Greyball%20logo.ico" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdn.rawgit.com/GreyballDevelopment/Website/master/main.css" type='text/css'>
+    <meta name="description" content="Greyball Minecraft Blag">
+        <meta name="keywords" content="Greyball, Blag, Minecraft">
+        <meta name="author" content="Joshua Merrill, Jacob Merrill">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Minecraft | Nebula Development | Blog</title>
   </head>
   <body>
--- Indexes/index-main.php
+++ Indexes/index-main.php
@@ -2,8 +2,12 @@
 <html lang='en-us'>
     <head>
         <meta charset='utf-8'/>
-        <link rel="icon" href="https://cdn.rawgit.com/GreyballDevelopment/Website/master/Greyball%201568x1568.ico" type="image/x-icon">
+        <link rel="icon" href="https://cdn.rawgit.com/GreyballDevelopment/Website/master/Icons/Greyball%20logo.ico" type="image/x-icon">
         <link rel="stylesheet" href="https://cdn.rawgit.com/GreyballDevelopment/Website/master/main.css" type='text/css'>
+        <meta name="description" content="Greyball main page">
+        <meta name="keywords" content="Development">
+        <meta name="author" content="Joshua Merrill, Jacob Merrill">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Welcome - Greyball</title>
         <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
         <script>
