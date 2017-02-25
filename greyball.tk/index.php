bdfa25944942464bdbcd226351673d43e8c2848d
Joshua Merrill <jsmerrill2004@gmail.com>
2017-02-24 21:03:00 -0800

    Update pages

--- greyball.tk/index.php
+++ greyball.tk/index.php
@@ -2,34 +2,17 @@
 <html lang='en-us'>
     <head>
         <meta charset='utf-8'/>
-        <link rel="icon" href="https://cdn.rawgit.com/GreyballDevelopment/Website/master/Icons/Greyball%20logo.ico" type="image/x-icon">
+        <link rel="icon" href="https://cdn.rawgit.com/GreyballDevelopment/Website/master/Icons/Greyball.ico" type="image/x-icon">
         <link rel="stylesheet" href="https://cdn.rawgit.com/GreyballDevelopment/Website/master/Styles/main.css" type='text/css'>
         <meta name="description" content="Greyball main page">
         <meta name="keywords" content="Development">
         <meta name="author" content="Joshua Merrill, Jacob Merrill">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Welcome - Greyball</title>
-        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-        <script>
-          (adsbygoogle = window.adsbygoogle || []).push({
-            google_ad_client: "ca-pub-2683826858268042",
-            enable_page_level_ads: true
-          });
-        </script>
     </head>
     <body>
         <p>Welcome to Greyball! You can find code for some of our games on <a href='https://github.com/GreyballDevelopment'>GitHub</a>.</p>
         <footer>
-            <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-            <!-- Responsive ad 1 -->
-            <ins class="adsbygoogle"
-                 style="display:block"
-                 data-ad-client="ca-pub-2683826858268042"
-                 data-ad-slot="8540436210"
-                 data-ad-format="auto"></ins>
-            <script>
-                (adsbygoogle = window.adsbygoogle || []).push({});
-            </script>
         </footer>
     </body>
 </html>
