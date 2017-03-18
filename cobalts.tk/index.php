33c2d38389e74483e081aa39a7e9c2e4c44869aa
Joshua Merrill <jsmerrill2004@gmail.com>
2017-03-17 20:29:42 -0700

    Do some renaming
    Renamed a bunch of files

---
+++ cobalts.tk/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang='en-us'>
+    <head>
+        <meta charset='utf-8'/>
+        <link rel="icon" href="https://cdn.rawgit.com/CobaltsDev/Website/master/Icons/Cobalts.ico" type="image/x-icon">
+        <link rel="stylesheet" href="https://cdn.rawgit.com/CobaltsDev/Website/master/Styles/main.css" type='text/css'>
+        <meta name="description" content="Cobalts main page">
+        <meta name="keywords" content="Development">
+        <meta name="author" content="Joshua Merrill, Jacob Merrill">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Welcome - Cobalts</title>
+    </head>
+    <body>
+        <p>Welcome to Cobalts! You can find code for some of our games on <a href='https://github.com/CobaltsDev'>GitHub</a>.</p>
+        <footer>
+        </footer>
+    </body>
+</html>
